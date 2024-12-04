51c46f8cb817cbf7dfec5fc407960fb05c67557e
Yehoshua Holstein <holsteinyehoshua@gmail.com>
2024-12-04 14:04:53 -0500

    Formatted final commit

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -13,27 +13,27 @@ public class CustomArrayList<T> implements CustomList<T> {
 		if (items == null) {
 			items[0] = item;
 			updateSuccess = true;
-		} else if (items[items.length -1] != null) {
-					Object[] newItems = increaseArraySizeIfNeeded();
+		} else if (items[items.length - 1] != null) {
+			Object[] newItems = increaseArraySizeIfNeeded();
 		}
-		for (int i = 0; i < items.length; i++ ) {
+		for (int i = 0; i < items.length; i++) {
 			if (items[i] == null) {
-				items[i] = item; 
+				items[i] = item;
 				updateSuccess = true;
 				break;
 			} else {
 				continue;
-				} 
+			}
 		}
-		return updateSuccess;  
+		return updateSuccess;
 	}
 
 	@Override
 	public int getSize() {
 		int elementsCount = 0;
-		for (int i = 0; i < items.length; i++ ) {
+		for (int i = 0; i < items.length; i++) {
 			if (items[i] != null) {
-				elementsCount++; 
+				elementsCount++;
 			} else {
 				continue;
 			}
@@ -47,14 +47,14 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return (T) items[index];
 	}
 
-	public Object[] increaseArraySizeIfNeeded () {
+	public Object[] increaseArraySizeIfNeeded() {
 		int arraySize = items.length;
-		Object[] moreItems = new Object[arraySize * 2]; 
+		Object[] moreItems = new Object[arraySize * 2];
 		for (int i = 0; i < items.length; i++) {
 			moreItems[i] = items[i];
 		}
 		items = moreItems;
 		return items;
 	}
-	
+
 }
