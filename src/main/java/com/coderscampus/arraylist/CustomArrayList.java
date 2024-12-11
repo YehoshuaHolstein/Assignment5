ed891c4b36de99a0bdd591d5b703cc7c03bbfc79
Yehoshua Holstein <holsteinyehoshua@gmail.com>
2024-12-11 13:56:39 -0500

    Final approved commit

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -10,13 +10,12 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	@Override
 	public boolean add(T item) {
-		boolean updateSuccess = false;
 		if (items[items.length - 1] != null) {
-			increaseArraySizeIfNeeded();
+			resizeArray();
 		}
 		items[size] = item;
 		size++;
-		return updateSuccess = true;
+		return true;
 	}
 
 	@Override
@@ -27,17 +26,22 @@ public class CustomArrayList<T> implements CustomList<T> {
 	@SuppressWarnings("unchecked")
 	@Override
 	public T get(int index) {
+		if (index < 0 || index >= items.length) {
+            throw new IndexOutOfBoundsException("Index: " + index + " is out of bounds for length: " + items.length);
+        }
 		return (T) items[index];
 	}
 		 
-	public void increaseArraySizeIfNeeded() {
-		int arraySize = items.length;
-		Object[] moreItems = new Object[arraySize * 2];
-		for (int i = 0; i < items.length; i++) {
-			moreItems[i] = items[i];
-		}
-		System.out.println("Array increased to length: " + moreItems.length);
-		items = moreItems;
+	public void resizeArray() {
+//		Object[] moreItems = new Object[items.length * 2];
+//		for (int i = 0; i < items.length; i++) {
+//			moreItems[i] = items[i];
+//		}
+//		System.out.println("Array increased to length: " + moreItems.length);
+//		items = moreItems;
+		
+		items = Arrays.copyOf(items, items.length * 2);
+		
 	}
 
 }
--- src/main/java/com/coderscampus/arraylist/CustomListApplication.java
+++ src/main/java/com/coderscampus/arraylist/CustomListApplication.java
@@ -7,17 +7,19 @@ public class CustomListApplication {
 	public static void main(String[] args) {
 		CustomList<String> myCustomList = new CustomArrayList<>();
 		for (int i = 0; i < 52; i++) {
-			boolean elementAdd = myCustomList.add("element " + i);
+			myCustomList.add("element " + i);
 		}
 		System.out.println("Total number of elements in array: " + myCustomList.getSize());
 		for(int i = 0; i < myCustomList.getSize(); i++)	{
 			System.out.println("i = " + i + " " + myCustomList.get(i));
 		}
+		
 		try {
 		    System.out.println(myCustomList.get(-1));
 		} catch (IndexOutOfBoundsException e) {
 		    System.out.println("Caught exception: " + e.getMessage());
 		}
+		
 		try {
 		    System.out.println(myCustomList.get(100));
 		} catch (IndexOutOfBoundsException e) {
