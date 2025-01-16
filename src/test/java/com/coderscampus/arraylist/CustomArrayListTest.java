ad75c69a756b01185406911a8b6c1d2fa673f246
Yehoshua Holstein <holsteinyehoshua@gmail.com>
2025-01-16 17:12:48 -0500

    1st update commit

---
+++ src/test/java/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -0,0 +1,19 @@
+package com.coderscampus.arraylist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomArrayListTest {
+	// Test-Driven Development Methodology
+	// Step 1 - Write a failing test
+	// Step 2 - Write the business logic to make the test pass
+	// Step 3 - Refactor your code
+	
+	@Test
+	void should_add_one_item_to_list() {
+		// Three A's
+		// Arrange, Act, Assert
+	}
+
+}
